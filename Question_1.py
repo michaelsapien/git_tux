548696909bb47d485f4abd1f9fb74e29261ee91c
anoopsapien <anoopsapien@gmail.com>
2024-03-07 13:42:32 +0530

    first commit

---
+++ Question_1.py
@@ -0,0 +1,30 @@
+"""
+create a new dictionary by extracting data from data,json
+new dictionay thus created should be as shown below
+
+
+[
+    {
+     "name": "Organic Honeycrisp Apples Bag",
+     "categories": ["Produce","Fruits","Apples",
+     "image":"https://d2d8wwwkmhfcva.cloudfront.net/800x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_fda52644-51b7-40cd-8322-c698b7ea30c1.png",
+     "base_price":0.64,
+     "availability_status":true
+    },
+    {
+     "name": "Granny Smith Apples",
+     "categories": ["Produce","Fruits","Apples",
+     "image":"https://d2d8wwwkmhfcva.cloudfront.net/800x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_fda52644-51b7-40cd-8322-c698b7ea30c1.png",
+     "base_price":0.64,
+     "availability_status":true
+    }
+    .
+    .
+    .
+    .
+    
+]
+
+
+Remember to extract all items above illustrated are just sample
+"""
